9aaa489a01b355b96bbb77f23354a6402a5675c2
jesw003 <kwang029@ucr.edu>
2018-03-07 14:54:06 -0800

    Add files via upload

---
+++ debug/helper_debug.cpp
@@ -0,0 +1,270 @@
+#include "helper.h"
+
+void truncate(char*& argv) {
+    // General level 3 depth - ";)
+    if (hasEndParenthesis(argv) && isAttached(argv) && isQuoteEnd(argv)) {
+        argv[strlen(argv) - 3] = ';';
+        argv[strlen(argv) - 2] = ')';
+    }
+    // General level 2 depth - ") or ;) or ";
+    else if (argv[strlen(argv) - 2] == ';' && hasEndParenthesis(argv)) {
+        argv[strlen(argv) - 2] = ')';
+    }
+    else if (argv[strlen(argv) - 2] == '"' && hasEndParenthesis(argv)) {
+        argv[strlen(argv) - 2] = ')';
+    }
+    else if (argv[strlen(argv) - 2] == '"' && isAttached(argv)) {
+        argv[strlen(argv) - 2] = ';';
+    }
+    argv[strlen(argv) - 1] = '\0';
+    return;
+}
+
+void trim(char*& argv) {
+    argv[0] = '\0';
+    ++argv;
+    return;
+}
+
+list<char*> parse(char cstrcmd[]) {
+    char* token;
+    list<char*> tokens;
+
+    token = strtok(cstrcmd, " ");
+    while (token != '\0') {
+        // cout << token << " Tokenized!" << endl;
+        tokens.push_back(token);
+        token = strtok('\0', " ");
+    }
+    return tokens;
+}
+
+void printArg(char* argv) {
+    for (int i = 0; argv[i] != '\0'; ++i) {
+        cout << argv[i];
+    }
+    return;
+}
+
+void printArgs(char* argv[]) {
+   for (int i = 0; argv[i] != '\0'; ++i) {
+        
+        cout << '\''; printArg(argv[i]); cout << '\'';
+        if (argv[i+1] != '\0') cout << ' ';
+    }
+}
+
+void printInfo(char* login, char host[]) {
+    printArg(login); 
+    cout << '@';
+    printArg(host);
+    return;
+}
+
+void printQueue(list<char**> q) {
+    for (int i = 0; !q.empty(); ++i) {
+        cout << "[DEBUG] Argument #" << i << ": ";
+        printArgs(q.front());
+        q.pop_front();
+        cout << endl;
+    }
+    return;
+}
+
+void printQueue(list<char*> q) {
+    while (!q.empty()) {
+        cout << q.front() << ' ';
+        q.pop_front();
+    }
+    cout << endl;
+    return;
+}
+
+void printLine(int num) {
+    cout << "[DEBUG] ";
+    for (int i = 0; i < num; ++i) cout << '-';
+    cout << endl;
+}
+
+void setNull(char* cTokens[]) {
+    for (int i = 0; cTokens[i] != '\0'; ++i) {
+        for (int n = 0; cTokens[n] != '\0'; ++n) {
+            cTokens[i][n] = '\0'; // Handling dangling pointers
+        }
+        cTokens[i] = '\0';
+    }
+    return;
+}
+
+void cutEndSpaces(char* cstr) {
+    for (int i = 0; cstr[i] != '\0'; ++i) {
+        if (cstr[i] == ' ') cstr[i] = '\0';
+    }
+    return;
+}
+
+bool test(char* argv[]) {
+
+    // Forks our process and stores its ID into a variable "child id"
+    
+    printLine(20);
+
+    struct stat fileStat;
+
+    cout << "[DEBUG] Statting "; printArgs(argv); cout << "!" << endl;
+    if (argv[1][0] == '-') { // If this is a flag
+        cout << "[DEBUG] Flag detected!" << endl;
+        if (stat(argv[2], &fileStat) == 0) {
+            if (argv[1][1] == 'e') return true;
+            else if (argv[1][1] == 'f') return S_ISREG(fileStat.st_mode);
+            else if (argv[1][1] == 'd') return S_ISDIR(fileStat.st_mode);
+            else {
+                cout << "test failed: invalid flag passed in\n";
+                return false;
+            }
+        }
+        else return false;
+    }
+    cout << "[DEBUG] No flag detected." << endl;
+
+    printLine(20);
+
+    return (stat(argv[1], &fileStat) == 0); 
+}
+
+Connector* buildTree(list<char**>& args, list<char*>& cons) {
+    // Returns the head of the tree.
+    // Allocated data here can be deleted during cleanup via preorder deletion.
+    
+    printLine(30);
+
+    ConnectorFactory factory;
+    // cout << "[DEBUG] Throwing "; printArgs(args.front()); cout << " into leftCmd." << endl;
+    cout << "[DEBUG] Building left node branch." << endl;
+    Connector* leftCmd = factory.createBranch(args, cons); 
+    cout << "[DEBUG] Left node Subtree Built." << endl;
+    // if (leftCmd != NULL) cout << "[DEBUG] leftCmd holds "; printArgs(leftCmd->getCmd()); cout << endl;
+    if (args.empty()) {
+        cout << "[DEBUG] args list held one element only, returning..." << endl;
+        return leftCmd;
+    }
+
+    Connector* rightCmd = NULL; 
+    Connector* currHead = NULL;
+    
+    while (!cons.empty()) {
+        while (!args.empty()) {
+
+            if (checkConnectors(cons.front()) == 1) { // If is hash, EVACUATE.
+                while (!args.empty()) args.pop_front();
+                while (!cons.empty()) cons.pop_front();
+                break;
+            }
+            currHead = factory.createConnector(checkConnectors(cons.front()));
+            cout << "[DEBUG] currHead speaking: "; currHead->identify();
+            cons.pop_front();
+
+            rightCmd = factory.createBranch(args, cons); // This is going to handle precedence.
+            cout << "[DEBUG] Branch created." << endl;
+
+            currHead->setLeft(leftCmd);
+            currHead->setRight(rightCmd);
+            cout << "[DEBUG] Statement: leftCmd = currHead" << endl;
+            leftCmd = currHead; // Sets left leaf to be current head.
+            cout << "[DEBUG] leftCmd: "; leftCmd->identify();
+        }
+        if (args.empty() && !cons.empty()) cons.pop_front();
+    }
+   
+    printLine(30);
+
+    return currHead;
+}
+
+void printTree(Connector* node) {
+    cout << "[DEBUG] I'm your current node. "; node->identify();
+    if (node->getLeft() != NULL) {
+        cout << "[DEBUG] I'm handing it over to my left branch!\n";
+        printTree(node->getLeft());
+    }
+    if (node->getRight() != NULL) {
+        cout << "[DEBUG] I'm now handing it over to my right branch!\n";
+        printTree(node->getRight());
+    }
+}
+
+
+int checkConnectors(char* argv) {
+    // This will be returning flags.
+    if (strcmp(argv, ";") == 0 || isAttached(argv)) return 0;
+    else if (strcmp(argv, "#") == 0) return 1;
+    else if (strcmp(argv, "||") == 0) return 2;
+    else if (strcmp(argv, "&&") == 0) return 3;
+    else return -1;
+}
+
+bool isConnector(char* argv) {
+    if (checkConnectors(argv) >= 0) return true;
+    return false;
+}
+
+bool isDblConnector(char* argv) {
+    int flag = checkConnectors(argv);
+    if (flag == 2 || flag == 3) return true;
+    return false;
+}
+
+bool isAttached(char* argv) {
+    if (strlen(argv) < 1) return false;
+    else if (argv[strlen(argv) - 1] == ';') return true;
+    else if (argv[strlen(argv) - 2] == ';' && hasEndParenthesis(argv)) return true;
+    return false;
+}
+
+bool isComment(char* argv) {
+    return (argv[0] == '#') ? true : false;
+}
+
+bool isQuoteBegin(char* argv) {
+    if (strlen(argv) <= 1) return false;
+    return (argv[0] == '"') ? true : false;
+}
+
+bool isQuoteEnd(char* argv) {
+    if (argv[strlen(argv) - 1] == '"') return true;
+    else if (hasEndParenthesis(argv) && argv[strlen(argv) - 2] == '"') return true;
+    else if (isAttached(argv) && (argv[strlen(argv) - 3] == '"' || argv[strlen(argv) - 2] == '"')) return true;
+    return false;
+}
+
+bool isTestBegin(char* argv) {
+    return (strcmp(argv, "[") == 0) ? true : false;
+}
+
+bool isTestEnd(char* argv) {
+    if (strcmp(argv, "]") == 0) return true;
+    else if (strcmp(argv, "])") == 0) return true;
+    return false;
+}
+
+bool hasStartParenthesis(char*& arg) {
+    if (arg[0] == '(') {
+        trim(arg);
+        return true;
+    }
+    return false;
+}
+
+bool hasEndParenthesis(char* args) {
+    return (args[strlen(args) - 1] == ')') ? true : false;
+}
+
+bool hasEndParenthesis(char**& args) {
+    for (int i = 0; args[i] != NULL; ++i) {
+        if (args[i][strlen(args[i]) - 1] == ')') {
+            truncate(args[i]);
+            return true;
+        }
+    }
+    return false;
+}
---
+++ debug/rshell_debug.cpp
@@ -0,0 +1,188 @@
+#include "include.h"
+
+#include "helper.h"
+#include "check.h"
+#include "connector.h"
+
+int main(int argc, char* argv[], char *envp[]) {
+   
+    // If you use execvp() without a fork, it'll take over the main system.
+    // This is why you need to fork a process, then call execvp()
+
+    list<char**> arguments;
+    list<char*> connectors;
+
+    // Grabs host name
+    char* login;
+    char host[127];
+
+    login = getlogin();
+    if (gethostname(host, 127) != 0) {
+        perror("failed to gethostname");
+        exit(EXIT_FAILURE);
+    }
+   
+    string command = "";
+
+    printInfo(login, host);
+    cout << "$ ";
+    while (getline(cin, command)) {
+
+        while (command.length() == 0) {
+            printInfo(login, host);
+            cout << "$ ";
+            getline(cin,command);
+        }
+
+        char* cstrcmd = '\0';
+        list<char*> tokens;
+        list<char*> data;
+
+        cstrcmd = new char[command.size() + 1]; 
+        // This data is universal. It will be used until deallocated.
+
+        strcpy(cstrcmd, command.c_str());
+        data.push_back(cstrcmd);
+        // This is to hold locations of the data to deallocate later
+        
+        tokens = parse(cstrcmd);
+
+        /*if (isDblConnector(tokens.back())) {
+            tokens.pop_back();
+            cout << "Terminated without command -- ignoring leading connector."
+                 << endl;
+        }*/
+
+        while (isDblConnector(tokens.back())) { 
+            // If the stupid user ended with a connector
+            list<char*> tmpCmds;
+
+            cout << "> ";
+            getline(cin, command);
+
+            cstrcmd = new char[command.size() + 1];
+            strcpy(cstrcmd, command.c_str());
+            data.push_back(cstrcmd);
+
+            tmpCmds = parse(cstrcmd);
+            while (!tmpCmds.empty()) {
+                tokens.push_back(tmpCmds.front());
+                tmpCmds.pop_front();
+            }
+        }
+
+        cout << "[DEBUG] Data.size(): " << data.size() << endl;
+
+        printLine(40);
+
+        // Creating the two argument and connector lists
+        int last; // There definitely has to be a better way to do this.
+        char* cmd[128][256]; 
+        // cmd is a list of pointers to the beginning of each token of the 
+        // universal data
+        int n = 0;
+        for (int i = 0; !tokens.empty(); ++i) {
+            cutEndSpaces(tokens.front());
+            if (isQuoteBegin(tokens.front())) { // Quote parsing
+                trim(tokens.front());
+                while (!isQuoteEnd(tokens.front())) { // Input clause should prevent infinite loop
+                    cout << "[DEBUG] cmd[" << n << "][" << i << "] = "; printArg(tokens.front()); cout << endl;
+                    cmd[n][i] = tokens.front();
+                    tokens.pop_front();
+                    ++i;
+                }
+                cout << "[DEBUG] Before truncation: "; printArg(tokens.front()); cout << endl;
+                truncate(tokens.front()); // Cuts off the quotation mark
+                cout << "[DEBUG] After truncation: "; printArg(tokens.front()); cout << endl;
+                if (isAttached(tokens.front())) { // Same clause as below 
+                    cout << "[DEBUG] Before truncation: "; printArg(tokens.front()); cout << endl;
+                    truncate(tokens.front()); // This one should cut off the semicolon 
+                    cout << "[DEBUG] After truncation: "; printArg(tokens.front()); cout << endl;
+                    cmd[n][i] = tokens.front(); 
+                    tokens.front() = new char[1]; 
+                    data.push_back(tokens.front());
+                    strcpy(tokens.front(), ";\0"); 
+                    tokens.push_front(NULL); // Phony Value
+                }
+                else {
+                    cmd[n][i] = tokens.front(); // Throws it into the list
+                }
+                if (tokens.front() != '\0') { 
+                    cout << "[DEBUG] Current tokens.front(): "; printArg(tokens.front()); cout << endl;
+                }
+                else {
+                    cout << "[DEBUG] Current tokens.front(): NULL" << endl;
+                }
+                ++i;
+            }
+            else if (isComment(tokens.front())) break;
+            else if (isConnector(tokens.front())) {
+                if (isAttached(tokens.front())) { 
+                    // Pesky semicolons being attached...
+                    truncate(tokens.front()); // Cuts out the semicolon, leaves rest of stack 
+                    cmd[n][i] = tokens.front(); 
+                    tokens.front() = new char[1]; 
+                    data.push_back(tokens.front());
+                    strcpy(tokens.front(), ";\0"); 
+                    ++i; // Progressing to the next value of cmd
+                }
+                cmd[n][i] = '\0'; // Null terminating cmd
+                arguments.push_back(cmd[n]); 
+                // If we ran into a connector, means we have a full argument.
+                connectors.push_back(tokens.front());
+              
+                ++n;
+                i = -1; // Reset argc
+            }
+            else if (isTestBegin(tokens.front()) && i == 0) { // Essentially converting "[" to "test"
+                cmd[n][0] = new char[4];
+                strcpy(cmd[n][0], "test");
+                data.push_back(cmd[n][0]); // potential glibc
+                cout << "[DEBUG] Popping "; printArg(tokens.front());
+                tokens.pop_front(); // Killing "[" from tokens.
+                for (i = 1; !isTestEnd(tokens.front()); ++i) {
+                    cout << "[DEBUG] cmd[" << n << "][" << i << "] = "; printArg(tokens.front()); cout << endl;
+                    cmd[n][i] = tokens.front();
+                    tokens.pop_front();
+                }
+            }
+            else {
+                cmd[n][i] = tokens.front(); 
+                // Assigns cmd[i] to pointer at tokens.front(). 
+                // Tokens contains cstrings
+            }
+            tokens.pop_front(); // Should take care of "]"
+            last = i;
+        }
+        if (cmd[n][0] != '\0') { // Push in the last argument
+            cmd[n][last + 1] = '\0';
+            arguments.push_back(cmd[n]);
+        }
+
+        // Debug printing:
+        cout << "[DEBUG] Contents of 'arguments': " << endl; printQueue(arguments);
+        cout << "[DEBUG] Contents of 'connectors': "; printQueue(connectors);
+
+        Connector* head = NULL;
+        head = buildTree(arguments, connectors);
+        cout << "[DEBUG] Tree built successfully... possibly!" << endl;
+        cout << "[DEBUG] head speaking: "; head->identify();
+        printLine(20); printTree(head); printLine(20);
+        head->run();
+
+        while (!connectors.empty()) connectors.pop_front();
+        while (!arguments.empty()) arguments.pop_front(); 
+        // If, for some reason, these two are not empty.
+        while (!data.empty()) {
+            cout << "[DEBUG] Deleting "; printArg(data.front()); cout << endl;
+            delete[] data.front();
+            // This deallocates the entire command string.
+            data.pop_front();
+        }
+        head->destroyBranch(head);
+        printLine(40);
+        printInfo(login, host);
+        cout << "$ ";
+    }
+    return 0;
+}
